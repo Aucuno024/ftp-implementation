257634e0b185ef7ce010ee6449ec177e5f366c37
aucuno <aucuno2012@gmail.com>
2026-03-16 16:25:50 +0100

    Traitant des requetes
    src/request.c (feat) ajout d'une fonction read_request pour lire une
    requete depuis l'entree d'un socket

---
+++ src/request.c
@@ -0,0 +1,14 @@
+#include "request.h"
+#include "csapp.h"
+#include <stdint.h>
+
+int read_request(request_t *request, int connfd)
+{
+    size_t n = 0;
+    request = malloc(sizeof(request_t));
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    n = Rio_readnb(&rio, &request, sizeof(request_t));
+    return n == sizeof(request_t);
+}
