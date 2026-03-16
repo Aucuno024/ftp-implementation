35c7157c61db13c9a6bb86a47110958da4093dd1
aucuno <aucuno2012@gmail.com>
2026-03-16 16:31:12 +0100

    include/request.h (feat) declaration de read_request

--- include/request.h
+++ include/request.h
@@ -1,6 +1,7 @@
 #ifndef __REQUEST_H__
 #define __REQUEST_H__
 #include "csapp.h"
+#include <stdint.h>
 
 typedef enum {
     GET = 0,
@@ -10,7 +11,16 @@ typedef enum {
 } typereq_t;
 
 typedef struct {
+    uint8_t endian;
     typereq_t typereq;
     char path[MAXLINE];
 } request_t;
+/**
+ * @fn int read_request(request_t *request, int connfd)
+ * @brief lit une requete depuis un descripteur de fichier
+ * @param request la variable ou ecrire ce qui est lu du descripteur
+ * @param connfd le descripteur de fichier
+ * @return 1 si il n'y pas d'erreur 0 sinon.
+ */
+int read_request(request_t *request, int connfd);
 #endif
