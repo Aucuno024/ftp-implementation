b6cd3c03b623c1e4ce470ed6da6faf761f602653
Kevin <kevin.dufaut@etu.univ-grenoble-alpes.fr>
2026-03-17 15:26:19 +0100

    Ajout d'un parser pour la commande du client

--- src/client.c
+++ src/client.c
@@ -6,6 +6,38 @@
 #include "response.h"
 #define PORT 2121
 
+
+
+/**
+ * @brief Analyse une commande entrée par l'utilisateur et remplit les champs typereq et path
+ * @param cmd la commande entrée par l'utilisateur
+ * @param typereq  un pointeur vers le type de requete a remplir
+ * @param path  un pointeur vers le chemin a remplir
+ * @return 0 si la commande est valide et a été parsée, 1 si le nom de la commande est invalide, 2 si le format de la commande est incorrect 
+ */
+int command_parser(const char *cmd, typereq_t *typereq, char *path) {
+    if (cmd == NULL || typereq == NULL || path == NULL) {
+        return 1;
+    }
+    uint8_t command[MAXLINE];
+    if (sscanf(cmd, "%s %s", command, path) != 2) {
+        return 2;
+    }
+
+    if (strcmp((char *) command, "get") == 0) {
+        *typereq = GET;
+    } else if (strcmp((char *) command, "put") == 0) {
+        *typereq = PUT;
+    } else if (strcmp((char *) command, "ls") == 0) {
+        *typereq = LS;
+    } else if (strcmp((char *) command, "rm") == 0) {
+        *typereq = RM;
+    } else {
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     int clientfd;
@@ -33,16 +65,26 @@ int main(int argc, char **argv)
     
     request_t request;
     response_t response;
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
-        size_t n = strlen(buf);
-        if (n > 0 && buf[n-1] == '\n') buf[n-1] = '\0';
-        encode_request(&request, GET, buf);
-        write_request(&request, clientfd);
-        
-        if (read_response(&response, clientfd) == 0) {
+    if (Fgets(buf, MAXLINE, stdin) != NULL) {
+        typereq_t typereq;
+        int err = command_parser(buf, &typereq, buf);
+        if (err != 0) {
+            printf("Commande invalide : ");
+            if (err == 2) {
+                printf("Format incorrect. Usage: <command> <path>\n");
+            } else if (err == 1) {
+                printf("Nom de commande invalide. Commandes valides: get, put, ls, rm\n");
+            }
+        } else {
+            size_t n = strlen(buf);
+            if (n > 0 && buf[n-1] == '\n') buf[n-1] = '\0';
+            encode_request(&request, typereq, buf);
+            write_request(&request, clientfd);
+            
+            // Lecture de la réponse du serveur
             uint8_t content[MAXLINE];
-            if (decode_response(&response, content) == 0) {
-                printf("Réponse: %s", content);
+            if (read_response(&response, clientfd) == 0 && decode_response(&response, content) == 0) {
+                printf("Reponse: %s", content);
             }
         }
     }
