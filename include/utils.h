81c4e13b058328c56749e401f170b987a28538d7
Kevin <kevin.dufaut@etu.univ-grenoble-alpes.fr>
2026-03-17 16:29:00 +0100

    Ajout d'une fonction `write_file_from_content` dans utils

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,6 @@
 #ifndef __UTILS_H__
 #define __UTILS_H__
+#include "csapp.h"
 
 /**
  * @brief Retourne l'endianness de la machine
@@ -14,4 +15,15 @@ int get_endianess();
  * @return 1 si tout s'est bien passé 0
  */
 int open_file_r(char path[], int *fd);
+
+
+/**
+ * @brief Ecrit le contenu dans un fichier a un chemin donnee
+ * @param path le chemin vers le fichier
+ * @param content le contenu a ecrire dans le fichier
+ * @return 1 si tout s'est bien passé 0 sinon
+ */
+int write_file_from_content(char path[], const uint8_t *content);
+
+
 #endif
--- src/utils.c
+++ src/utils.c
@@ -12,3 +12,13 @@ int open_file_r(char path[], int *fd)
     return (*fd = open(path, O_RDONLY, 0)) != -1;
 }
 
+int write_file_from_content(char path[], const uint8_t *content)
+{
+    int fd;
+    if ((fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
+        return 0;
+    }
+    write(fd, content, strlen((char *)content));
+    close(fd);
+    return 1;
+}
