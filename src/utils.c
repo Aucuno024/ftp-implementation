cee484ab7ee1608b2b3116b9733d362129c59f38
Kevin <kevin.dufaut@etu.univ-grenoble-alpes.fr>
2026-03-23 15:44:32 +0100

    ajout d'une sécurité pour open_file_r Les fichier demandés doivent être dans le dossier du serveur, sinon le chemin est considéré comme invalide

--- src/utils.c
+++ src/utils.c
@@ -9,26 +9,69 @@ int get_endianess() {
     return ((* (uint8_t *) &one) == 0);
 }
 
+/**
+ * @brief Vérification si le fichier a ouvrir est dans le dossier du serveur
+ * @param abs_path le chemin absolu du fichier a ouvrir
+ * @return int 1 si le fichier est dans le dossier du serveur, 0 sinon
+ */
+int is_path_in_server_dir(const char *abs_path) {
+    char server_dir_abs[MAXLINE];
+    if (realpath(DEFAULT_SERVER_DIR, server_dir_abs) == NULL) {
+        return 0;
+    }
+    size_t dir_len = strlen(server_dir_abs);
+    return strncmp(abs_path, server_dir_abs, dir_len) == 0 && (abs_path[dir_len] == '/' || abs_path[dir_len] == '\0');
+}
+
+
+/** 
+ * @brief Convertir un chemin en chemin absolu depuis le dossier du serveur
+ * @param path le chemin à convertir
+ * @param abs_path le buffer où stocker le chemin absolu converti
+ */
+void convert_to_abs_path(const char *path, char *abs_path) {
+    char temp_path[MAXLINE];
+    if (path == NULL || abs_path == NULL) {
+        abs_path[0] = '\0'; // chemin invalide
+        return;
+    }
+    if (path[0] == '/') {
+        // chemin absolu
+        snprintf(temp_path, MAXLINE, "%s%s", DEFAULT_SERVER_DIR, path + 1);
+    } else {
+        // chemin relatif
+        snprintf(temp_path, MAXLINE, "%s/%s", DEFAULT_SERVER_DIR, path);
+    }
+    // convertir le chemin du serveur en chemin absolu
+    if (realpath(temp_path, abs_path) == NULL) {
+        abs_path[0] = '\0'; // chemin invalide
+    }
+}
+/**
+ * @brief Fourni le chemin vers le serveur à partir d'un chemin donné, en vérifiant que le chemin résultant est bien dans le dossier du serveur
+ * @param path le chemin à convertir
+ * @param server_path le buffer où stocker le chemin vers le serveur
+ * @return int 1 si le chemin est valide et dans le dossier du serveur, 0 sinon
+ */
+int build_server_path(const char *path, char *server_path) {
+    convert_to_abs_path(path, server_path);
+    if (server_path[0] == '\0' || !is_path_in_server_dir(server_path)) {
+        return 0;
+    }
+    return 1;
+}
+
+
 int open_file_r(char path[], int *fd)
 {
-    if(is_relative_path(path))
-    {
-        char *newpath = malloc(strlen(path) + strlen(DEFAULT_SERVER_DIR) + 1);
-        int i;
-        for(i = 0; DEFAULT_SERVER_DIR[i] != '\0'; i++)
-        {
-            newpath[i] = DEFAULT_SERVER_DIR[i];
-        }
-        for(int j = 0; path[j] != '\0'; j++)
-        {
-            newpath[i++] = path[j];
-        }
-        newpath[i] = '\0';
-        int r = (*fd = open(newpath, O_RDONLY, 0)) != -1;
-        free(newpath);
-        return r;
+    if (path == NULL || fd == NULL) {
+        return 0;
     }
-    return (*fd = open(path, O_RDONLY, 0)) != -1;
+    char abs_path[MAXLINE];
+    if (!build_server_path(path, abs_path)) {
+        return 0;
+    }
+    return (*fd = open(abs_path, O_RDONLY, 0)) != -1;
 }
 
 int write_file_from_content(char path[], const uint8_t *content)
